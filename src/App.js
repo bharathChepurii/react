97b891eb8b11673a6c4fb637ba562714ec5c544b
bharathChepurii <bharathchepuri07@gmail.com>
2025-04-24 14:11:42 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from 'react'; 
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import Link from './components/Link';
+import { products } from './components/menuData';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Card />
+      <Link />
+
+
+      <div className="products-container">
+        <h2>Products</h2>
+        <div className="products-list">
+          {products.map((product, index) => (
+            <div key={index} className="product-card">
+              <h3>{product.value.items[0].value.cardType.contentCard.contentStoreCard.headline}</h3>
+              <img
+                src={product.value.items[0].value.cardType.contentCard.contentStoreCard.cardImage.srcSet.src}
+                alt={product.value.items[0].value.cardType.contentCard.contentStoreCard.cardImage.alt || 'Product Image'}
+                width="200"
+                height="130"
+              />
+              <a
+                href={product.value.items[0].value.cardType.contentCard.contentStoreCard.textLink.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn More
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Product.js
@@ -0,0 +1,7 @@
+function product(){
+    return (
+        <div className="product">
+            <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-chat-earth-day-specialist-icon-202504_AV2?wid=70&hei=70&fmt=jpeg&qlt=90&.v=1742997161934" alt="Product" width="100" height="100" />
+        </div>
+    )
+}
